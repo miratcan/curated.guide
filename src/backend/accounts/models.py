5675ee28efbae7af4fd5a6d57904ea8171120c15
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2025-11-22 21:40:10 +0300

    Add custom User model (accounts app)

---
+++ src/backend/accounts/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """Custom user model for curated.guide"""
+
+    # Future fields can be added here
+    # e.g., bio, avatar, custom_domain, etc.
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
